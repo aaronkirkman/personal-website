bb6421fda7b0825838eceb64e4cd64d87cf2f840
Aaron Kirkman <kirkman.aaron.w+github@gmail.com>
2020-06-28 17:31:01 -0400

    import initial website build

---
+++ build/privacy.md
@@ -0,0 +1,7 @@
+Title: Privacy: Aaron Kirkman
+
+
+# Google Fonts
+
+For consistency across devices, Google Fonts is used to load the fonts on this page.
+[Google says they do not track these requests](https://web.archive.org/web/20200426130508/https://developers.google.com/fonts/faq), but the site will still work if you block these fonts.
---
+++ site.py
@@ -0,0 +1,65 @@
+#!/usr/bin/python3
+# -*- coding: utf-8 -*-
+
+
+import configparser
+import datetime
+import jinja2
+import markdown2
+import os
+import pathlib
+import shutil
+
+
+# Load settings to get build and deployment directories
+config = configparser.ConfigParser()
+config.read("config.ini")
+build_dir = config["directories"]["build"]
+site_dir = config["directories"]["site"]
+
+
+# Archive old site directory
+old_site_dir = f"{site_dir}-prev"
+try:
+    shutil.rmtree(old_site_dir)
+except FileNotFoundError:
+    print("Old site directory not found")
+os.rename(site_dir, old_site_dir)
+
+
+# Create new site directory by copying build directory
+shutil.copytree(build_dir, site_dir)
+
+
+
+
+def get_extension(filename):
+    return(pathlib.Path(filename).suffix)
+
+
+
+# Load Jinja template
+with open("templates/main.html.j2", "r") as template_file:
+    template = jinja2.Template(template_file.read())
+
+
+
+for root, dirs, files in os.walk(site_dir):
+    for filename in files:
+        if get_extension(filename) == ".md":
+            markdown_file = os.path.join(root, filename)
+            print(markdown_file)
+            html_file = f"{pathlib.Path(markdown_file).with_suffix('')}.html"
+            print(html_file)
+
+            # Render Markdown to HTML
+            with open(markdown_file, "r") as mf:
+                html = markdown2.markdown(mf.read(), extras=["metadata"])
+                html_output = template.render(title = html.metadata["Title"],
+                                              content = html,
+                                              last_updated = datetime.date.today().strftime("%Y-%m-%d"))
+
+            with open(html_file, "w") as hf:
+                hf.write(html_output)
+
+            os.remove(markdown_file)
